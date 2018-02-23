77bd8e921022b84bcae2d4efbe1d6ecc2fc1a8dc
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2018-02-23 15:58:08 -0500

    Implement SkipList Insert for single-thread

--- src/include/index/skiplist.h
+++ src/include/index/skiplist.h
@@ -11,7 +11,10 @@
 //===----------------------------------------------------------------------===//
 
 #pragma once
-
+#include <vector>
+#include <utility>
+#include <functional>
+#include <iostream>
 namespace peloton {
 namespace index {
 
@@ -21,10 +24,140 @@ namespace index {
 #define SKIPLIST_TEMPLATE_ARGUMENTS                                       \
   template <typename KeyType, typename ValueType, typename KeyComparator, \
             typename KeyEqualityChecker, typename ValueEqualityChecker>
+
 template <typename KeyType, typename ValueType, typename KeyComparator,
           typename KeyEqualityChecker, typename ValueEqualityChecker>
 class SkipList {
-  // TODO: Add your declarations here
+ public:
+  using KeyValuePair = std::pair<KeyType, ValueType>;
+
+  ///////////////////////////////////////////////////////////////////
+  // Node Types
+  ///////////////////////////////////////////////////////////////////
+  class BaseNode {
+   public:
+    BaseNode *next;
+  };
+
+  class HeadNode : public BaseNode {};
+
+  class LeafNode : public BaseNode {
+   public:
+    KeyValuePair pair;
+  };
+
+  class InnerNode : public BaseNode {
+   public:
+    KeyType key;
+    BaseNode *down;
+  };
+
+  ///////////////////////////////////////////////////////////////////
+  // Key Comparison Member Functions
+  ///////////////////////////////////////////////////////////////////
+
+  /*
+   * KeyCmpLess() - Compare two keys for "less than" relation
+   *
+   * If key1 < key2 return true
+   * If not return false
+   */
+  inline bool KeyCmpLess(const KeyType &key1, const KeyType &key2) const {
+    return key_cmp_obj(key1, key2);
+  }
+
+  /*
+   * KeyCmpGreater() - Compare a pair of keys for > relation
+   *
+   * It flips input for keyCmpLess()
+   */
+  inline bool KeyCmpGreater(const KeyType &key1, const KeyType &key2) const {
+    return KeyCmpLess(key2, key1);
+  }
+
+  /*
+   * KeyCmpLessEqual() - Compare a pair of keys for <= relation
+   */
+  inline bool KeyCmpLessEqual(const KeyType &key1, const KeyType &key2) const {
+    return !KeyCmpGreater(key1, key2);
+  }
+
+  ////////////////////////////////////////////////////////////////////
+  // Interface Method Implementation
+  ////////////////////////////////////////////////////////////////////
+  bool Insert(const KeyType &key, const ValueType &value);
+  //  bool ConditionalInsert(const KeyType &key, const ValueType &value,
+  //                         std::function<bool(const void *)> predicate,
+  //                         bool *predicate_satisfied);
+  //  bool Delete(const KeyType &key, const ValueType &value);
+  //  void GetValue(const KeyType &search_key, std::vector<ValueType>
+  //  &value_list);
+  //
+  //  ///////////////////////////////////////////////////////////////////
+  //  // Garbage Collection
+  //  ///////////////////////////////////////////////////////////////////
+  //  bool NeedGarbageCollection();
+  //  void PerformGarbageCollection();
+  //
+  //  ///////////////////////////////////////////////////////////////////
+  //  // Forward Iterator
+  //  ///////////////////////////////////////////////////////////////////
+  //  /*
+  //   * Iterator Interface
+  //   */
+  //  class ForwardIterator;
+  //  ForwardIterator Begin();
+  //  ForwardIterator Begin(const KeyType &start_key);
+  //  bool IsEnd() const;
+  //  inline ForwardIterator &operator++();
+  //    ///////////////////////////////////////////////////////////////////
+  //    // Utility Funciton
+  //    ///////////////////////////////////////////////////////////////////
+  bool IsEmpty() { return head_nodes[0].next == NULL; }
+  void PrintSkipList();
+
+ public:
+  // Constructor
+  SkipList(KeyComparator p_key_cmp_obj = KeyComparator{},
+           KeyEqualityChecker p_key_eq_obj = KeyEqualityChecker{},
+           ValueEqualityChecker p_value_eq_obj = ValueEqualityChecker{})
+      : key_cmp_obj(p_key_cmp_obj),
+        key_eq_obj(p_key_eq_obj),
+        value_eq_obj(p_value_eq_obj) {
+    head_nodes.push_back(HeadNode());
+  };
+
+  // Destructor
+  ~SkipList() {
+    for (unsigned i = 1; i < head_nodes.size(); i++) {
+      InnerNode *cur = (InnerNode *)head_nodes[i].next;
+      InnerNode *prev = NULL;
+      while (cur != NULL) {
+        prev = cur;
+        cur = (InnerNode *)(cur->next);
+        delete prev;
+      }
+    }
+    LeafNode *cur = (LeafNode *)head_nodes[0].next;
+    LeafNode *prev = NULL;
+    while (cur != NULL) {
+      prev = cur;
+      cur = (LeafNode *)(cur->next);
+      delete prev;
+    }
+  }
+  // Key comparator
+  const KeyComparator key_cmp_obj;
+
+  // Raw key eq checker
+  const KeyEqualityChecker key_eq_obj;
+
+  const ValueEqualityChecker value_eq_obj;
+
+  std::vector<HeadNode> head_nodes;
+
+ private:
+  void *Search(const KeyType &key, int level);
 };
 
 }  // namespace index
--- src/index/skiplist.cpp
+++ src/index/skiplist.cpp
@@ -11,11 +11,132 @@
 //===----------------------------------------------------------------------===//
 
 #include "index/skiplist.h"
+#include <stdlib.h>
+#include <utility>
+#include <iostream>
+
+#define SKIPLIST_TYPE                                             \
+  SkipList<KeyType, ValueType, KeyComparator, KeyEqualityChecker, \
+           ValueEqualityChecker>
 
 namespace peloton {
 namespace index {
 
-// Add your function definitions here
+SKIPLIST_TEMPLATE_ARGUMENTS
+bool SKIPLIST_TYPE::Insert(const KeyType &key, const ValueType &value) {
+  // Check whether we should insert the new entry
+ 
+  // Determine the height of the tower
+  int levels = 0;
+  while (rand() % 2) levels++;
+
+  // Add additional levels if the tower exceeds the maximum height
+  if (levels > (head_nodes.size() - 1)) {
+    int diff = levels - head_nodes.size() + 1;
+    for (int i = 0; i < diff; i++) {
+      head_nodes.push_back(HeadNode());
+    }
+  }
+
+  // Fill in keys and values and then link the tower
+  LeafNode *lf_node = new LeafNode;
+  lf_node->pair = std::make_pair(key, value);
+  InnerNode *in_nodes[levels];
+  for (int i = 0; i < levels; i++) in_nodes[i] = new InnerNode;
+  if (levels > 0) {
+    for (int i = 0; i < levels - 1; i++) {
+      in_nodes[i]->key = key;
+      in_nodes[i]->down = in_nodes[i + 1];
+    }
+    in_nodes[levels - 1]->key = key;
+    in_nodes[levels - 1]->down = lf_node;
+  }
+
+  // Find the position to insert the key for each level
+  // TODO: Make this concurrent
+  void *ptr = Search(key, 0);
+  if (ptr == NULL) {
+    lf_node->next = head_nodes[0].next;
+    head_nodes[0].next = lf_node;
+  } else {
+    static_cast<LeafNode *>(ptr)->next = lf_node;
+  }
+
+  for (int i = 1; i <= levels; i++) {
+    void *ptr = Search(key, i);
+    if (ptr == NULL) {
+      in_nodes[i - 1]->next = head_nodes[i].next;
+      head_nodes[i].next = in_nodes[i - 1];
+    } else {
+      in_nodes[i - 1]->next = static_cast<InnerNode *>(ptr)->next;
+      static_cast<InnerNode *>(ptr)->next = in_nodes[i - 1];
+    }
+  }
+
+  return true;
+}
+
+SKIPLIST_TEMPLATE_ARGUMENTS
+void SKIPLIST_TYPE::PrintSkipList() {
+  std::cout << head_nodes.size() << std::endl;
+  for (int i = head_nodes.size() - 1; i > 0; i--) {
+    std::cout << "Level " << i << " :";
+    InnerNode *cur = static_cast<InnerNode *>(head_nodes[i].next);
+    while (cur != NULL) {
+      std::cout << cur->key << "--->";
+      cur = static_cast<InnerNode *>(cur->next);
+    }
+    std::cout << std::endl;
+  }
+  std::cout << "Level " << 0 << " :";
+  LeafNode *cur = static_cast<LeafNode *>(head_nodes[0].next);
+  while (cur != NULL) {
+    std::cout << "(" << cur->pair.first << ", " << cur->pair.second << ") --->";
+    cur = static_cast<LeafNode *>(cur->next);
+  }
+  std::cout << std::endl;
+}
+
+/* It returns the pointer to the node whose key is the largest key <= key at
+ * level specified by the input. (It returns a pointer to InnerNode if level
+ * > 0 and a pointer to LeafNode if level == 0)
+ * If the there is no node before the key at that level, it returns NULL.
+ * It returns NULL if level is invalid.
+ * */
+SKIPLIST_TEMPLATE_ARGUMENTS
+void *SKIPLIST_TYPE::Search(const KeyType &key, int level) {
+  // Check if skiplist is empty
+  if (IsEmpty()) return NULL;
+  if (level > head_nodes.size() - 1) return NULL;
+
+  // Keep track of the level
+  typename std::vector<HeadNode>::reverse_iterator it = head_nodes.rbegin();
+
+  int cur_level = head_nodes.size() - 1;
+  InnerNode *cur = (InnerNode *)head_nodes[cur_level].next;
+  void *prev = NULL;
+  while (1) {
+    if (cur_level == 0) {
+      LeafNode *leaf_cur = (LeafNode *)cur;
+      while (leaf_cur != NULL && KeyCmpLessEqual(leaf_cur->pair.first, key)) {
+        prev = leaf_cur;
+        leaf_cur = (LeafNode *)(leaf_cur->next);
+      }
+    } else {
+      while (cur != NULL && KeyCmpLessEqual(cur->key, key)) {
+        prev = cur;
+        cur = (InnerNode *)(cur->next);
+      }
+    }
+    // We finished searching at level cur_level
+    if (cur_level == level) return static_cast<void *>(prev);
+    cur_level--;
+    if (prev == NULL)
+      cur = (InnerNode *)head_nodes[cur_level].next;
+    else
+      cur = (InnerNode *)(((InnerNode *)prev)->down);
+  }
+}
 
 }  // namespace index
 }  // namespace peloton
